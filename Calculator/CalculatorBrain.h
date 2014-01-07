9dadf223dd7fc220ec996f1bb1cd58f6a6712755
Karthikeyan Ramanathan <Karthik@Karthik-MacBook-Pro.local>
2014-01-07 23:39:19 +0530

    Added public property program to CalculatorBrain

--- Calculator/CalculatorBrain.h
+++ Calculator/CalculatorBrain.h
@@ -13,4 +13,9 @@
 - (void) pushOperand:(double)operand;
 - (double) performOperation:(NSString *) operation;
 
+@property (readonly) id program;
+
++ (double) runProgram:(id)program;
++ (NSString *) descriptionOfTheProgram:(id)program;
+
 @end
